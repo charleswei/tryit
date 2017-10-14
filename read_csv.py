e8d6f0f1d92fca55f861f51507654112126f6cb4
charleswei <charleswei2008@gmail.com>
2017-10-15 02:23:45 +0800

    update

--- read_csv.py
+++ read_csv.py
@@ -10,8 +10,8 @@ df[u'HireDate']= pd.to_datetime(df[u'HireDate'],format='%Y-%m-%d')
 df1 = df.set_index('HireDate')
 df_t= df1
 #print df_t
-list_t=[len(df_t[str(2006)]),len(df_t[str(2006):str(2007)]),len(df_t[str(2006):str(2008)]),len(df_t[str(2006):str(2009)]),len(df_t[str(2006):str(2010)]),len(df_t[str(2006):str(2011)]),len(df_t[str(2006):str(2012)]),len(df_t[str(2006):str(2013)]),len(df_t[str(2006):str(2014)]),len(df_t[str(2006):str(2015)]),len(df_t[str(2006):str(2016)]),len(df_t[str(2006):str(2017)])]
-print list_t
+#list_t=[len(df_t[str(2006)]),len(df_t[str(2006):str(2007)])--len(df3['2006':'2007']),len(df_t[str(2006):str(2008)])-len(df3['2006':'2008']),len(df_t[str(2006):str(2009)])-len(df3['2006':'2009']),len(df_t[str(2006):str(2010)])-len(df3['2006':'2010']),len(df_t[str(2006):str(2011)])-len(df3['2006':'2011']),len(df_t[str(2006):str(2012)])-len(df3['2006':'2012']),len(df_t[str(2006):str(2013)])-len(df3['2006':'2013']),len(df_t[str(2006):str(2014)])-len(df3['2006':'2014']),len(df_t[str(2006):str(2015)])-len(df3['2006':'2015']),len(df_t[str(2006):str(2016)])-len(df3['2006':'2016']),len(df_t[str(2006):str(2017)])-len(df3['2006':'2017'])]
+#print list_t
 j=1
 list = [len(df1[str(2006)]),];
 
@@ -41,13 +41,16 @@ del list1[0]
 print list2
 print list1
 s2=Series(list1)
+
+list_t=[len(df_t[str(2006)]),len(df_t[str(2006):str(2007)])--len(df3['2006':'2007']),len(df_t[str(2006):str(2008)])-len(df3['2006':'2008']),len(df_t[str(2006):str(2009)])-len(df3['2006':'2009']),len(df_t[str(2006):str(2010)])-len(df3['2006':'2010']),len(df_t[str(2006):str(2011)])-len(df3['2006':'2011']),len(df_t[str(2006):str(2012)])-len(df3['2006':'2012']),len(df_t[str(2006):str(2013)])-len(df3['2006':'2013']),len(df_t[str(2006):str(2014)])-len(df3['2006':'2014']),len(df_t[str(2006):str(2015)])-len(df3['2006':'2015']),len(df_t[str(2006):str(2016)])-len(df3['2006':'2016']),len(df_t[str(2006):str(2017)])-len(df3['2006':'2017'])]
+
 s3=Series(list_t)
 
 plt.plot([0,1,2,3,4,5,6,7,8,9,10,11],s1,label="New Hires")
 plt.plot([0,1,2,3,4,5,6,7,8,9,10,11],s2,label="Leavers")
 plt.plot([0,1,2,3,4,5,6,7,8,9,10,11],s3,label="Associates")
 #plt.text(60, .025, r'$\mu=100,\ \sigma=15$')  
-plt.ylim(0,1500)
+plt.ylim(0,700)
 
 plt.xlabel('Year')
 plt.ylabel('Number of Associates')
