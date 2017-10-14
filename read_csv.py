1c5c2db3ecb76ebc98717dd0b4be04ebc384bcce
charleswei <charleswei2008@gmail.com>
2017-10-15 00:30:53 +0800

    update

--- read_csv.py
+++ read_csv.py
@@ -15,28 +15,33 @@ for i in range(2005,2017):
     list += [len(df1[str(i)]),]
     j +=1
     i +=1
+print list
 s1=Series(list)
+
 list2 = [2005,]
 df3 = df1[df1.TerminationDate <>'None']
 j = 1
 list1 = [len(df3[str(i)]),];
 for i in range(2005,2017):
-    print i+1
+   
     list2 += [(i+1),] 
     list1 += [len(df3[str(i)]),]
     j +=1
     i +=1
 del list2[0]
 print list2
+print list1
 s2=Series(list1)
 
-plt.plot(s1)
-plt.plot(s2)
+plt.plot(s1,label='New Hires')
+
+plt.plot(s2,label='Leavers')
+#plt.text(60, .025, r'$\mu=100,\ \sigma=15$')  
 plt.ylim(0,200)
 plt.xlabel('Year')
 plt.ylabel('Number of Associates')
 plt.title('The change of Associates')
-
-#plt.xticks('2006','2006','2006','2006','2006','2006','2006','2006','2006','2006','2006','2006','2006')
+plt.annotate('local max', xy=(10,125), xytext=(7, 125),arrowprops=dict(facecolor='red', shrink=0.01),)  
+plt.xticks([0,1,2,3,4,5,6,7,8,9,10,11],['2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017'])
 plt.show()
 
