2551d3ec8dae6c3171d5057e2d2704d8c4a6a721
charleswei <charleswei2008@gmail.com>
2017-10-15 02:13:04 +0800

    update

--- read_csv.py
+++ read_csv.py
@@ -6,7 +6,12 @@ import matplotlib.pyplot as plt
 from pylab import *
 df  = pd.read_csv('raycom-people.csv')
 df[u'HireDate']= pd.to_datetime(df[u'HireDate'],format='%Y-%m-%d')
+
 df1 = df.set_index('HireDate')
+df_t= df1
+#print df_t
+list_t=[len(df_t[str(2006)]),len(df_t[str(2006):str(2007)]),len(df_t[str(2006):str(2008)]),len(df_t[str(2006):str(2009)]),len(df_t[str(2006):str(2010)]),len(df_t[str(2006):str(2011)]),len(df_t[str(2006):str(2012)]),len(df_t[str(2006):str(2013)]),len(df_t[str(2006):str(2014)]),len(df_t[str(2006):str(2015)]),len(df_t[str(2006):str(2016)]),len(df_t[str(2006):str(2017)])]
+print list_t
 j=1
 list = [len(df1[str(2006)]),];
 
@@ -36,15 +41,17 @@ del list1[0]
 print list2
 print list1
 s2=Series(list1)
+s3=Series(list_t)
 
 plt.plot([0,1,2,3,4,5,6,7,8,9,10,11],s1,label="New Hires")
-
 plt.plot([0,1,2,3,4,5,6,7,8,9,10,11],s2,label="Leavers")
+plt.plot([0,1,2,3,4,5,6,7,8,9,10,11],s3,label="Associates")
 #plt.text(60, .025, r'$\mu=100,\ \sigma=15$')  
-#plt.ylim(0,200)
+plt.ylim(0,1500)
 
 plt.xlabel('Year')
 plt.ylabel('Number of Associates')
+
 plt.title('The change of Associates')
 plt.annotate('local max', xy=(9,125), xytext=(5, 125),arrowprops=dict(facecolor='red', shrink=0.01),)  
 plt.xticks([0,1,2,3,4,5,6,7,8,9,10,11],['2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017'])
